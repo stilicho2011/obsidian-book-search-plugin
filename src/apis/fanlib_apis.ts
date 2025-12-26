b180dbab62245c15f4f9f3deb0e62ddd8c210386
stilicho2011 <149189831+stilicho2011@users.noreply.github.com>
2025-12-26 10:48:06 +0300

    Update and rename fanlib.ts to fanlib_apis.ts

--- src/apis/fanlib_apis.ts
+++ src/apis/fanlib_apis.ts
@@ -1,9 +1,13 @@
 
-// src/apis/fanlib_api.ts
 import { requestUrl } from 'obsidian';
 import { Book } from '@models/book.model';
 
 export class FanlibAPI {
+  /**
+   * Поиск книги по ISBN через FantLab API
+   * @param isbn - ISBN книги
+   * @returns массив книг (Book[])
+   */
   static async searchByISBN(isbn: string): Promise<Book[]> {
     try {
       const url = `https://api.fantlab.ru/work?isbn=${encodeURIComponent(isbn)}`;
